5e782d79e8902a8441b9a7c598d68a80cf097c6d
Alessandro Dionisi <a.dionisi@voverc.com>
2020-03-18 09:15:08 +0100

    Fix

--- results.sh
+++ results.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 echo '--- Desk phone without override fragment ---'
-curl -sX GET http://localhost:8080/api/v1/provisioning/aa-bb-cc-dd-ee-ff | jq '.'
+curl -sX GET http://localhost:8080/api/v1/provisioning/aa-bb-cc-dd-ee-ff
 echo '--- Conference phone without override fragment ---'
 curl -sX GET http://localhost:8080/api/v1/provisioning/f1-e2-d3-c4-b5-a6 | jq '.'
 echo '--- Desk phone with override fragment ---'
-curl -sX GET http://localhost:8080/api/v1/provisioning/a1-b2-c3-d4-e5-f6 | jq '.'
+curl -sX GET http://localhost:8080/api/v1/provisioning/a1-b2-c3-d4-e5-f6
 echo '--- Conference phone with override fragment ---'
 curl -sX GET http://localhost:8080/api/v1/provisioning/1a-2b-3c-4d-5e-6f | jq '.'
