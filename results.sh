32c63b138008f7028dee0badbd5c30a841e71a9b
Alessandro Dionisi <a.dionisi@voverc.com>
2020-03-25 15:32:54 +0100

    Print not existing device

--- results.sh
+++ results.sh
@@ -7,3 +7,5 @@ echo '--- Desk phone with override fragment ---'
 curl -sX GET http://localhost:8080/api/v1/provisioning/a1-b2-c3-d4-e5-f6
 echo '--- Conference phone with override fragment ---'
 curl -sX GET http://localhost:8080/api/v1/provisioning/1a-2b-3c-4d-5e-6f | jq '.'
+echo '--- Not existing device ---'
+curl -w "%{http_code}" -sX GET http://localhost:8080/api/v1/provisioning/aa-aa-aa-aa-aa-aa
